a00228722a282a0a76099c22e87e0c60301d98e5
marciaalegria <ytasegundo@gmail.com>
2022-11-15 12:28:52 -0300

    consumiendo api ok

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { useState } from 'react';
 import './App.css';
 import Busqueda from './components/Busqueda';
 import Cards from './components/Cards';
 import FiltrosUniversos from './components/FiltrosUniversos';
+import Titulo from './components/Titulo';
+import Llamada from './Llamada';
+
 
 function App() {
   return (
     <div className="App">
       <>
+      <i class="bi bi-search"></i>
+      <i class="bi bi-funnel"></i>
+      <Llamada/>
       <Busqueda/>
       <FiltrosUniversos/>
-      <h2>DragonBall prueba del dragon</h2>
+      <Titulo/>
       <Cards/>
       </>
     </div>
---
+++ src/components/Titulo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Titulo
+() {
+  return (
+    <div>
+        <h2>imagen</h2>
+        <h1>Prueba del Dragón</h1>
+    </div>
+  )
+}
