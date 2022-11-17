93015f6870d6add1f7d812619593f41ff2f01ff3
marciaalegria <ytasegundo@gmail.com>
2022-11-17 17:12:08 -0300

    card-container css

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,32 +1,29 @@
 import React from 'react'
-
 import './Card.css'
 
 export default function Cards({personaje}) {
   return (
     <div className='cards-container'>
+      
       {personaje.map((item,index)=>(
         <div key={index}>
           <div className='card-container'>
-            <div className='card-title'>
+            {/* <div className='card-title'>
               <h2>{item.name}</h2>
-            </div>
-            <div className='card-universe'>
+            </div> */}
+            {/* <div className='card-universe'>
               <h3>{item.universe}</h3>
-            </div>
-            <div className='card-image'>
+            </div> */}
+            {/* <div className='card-image'>
               <img src={item.imageUrl} alt='personaje'></img>
-            </div>
-            <button>
+            </div> */}
+            {/* <button>
               <a href=''>Ver Ficha</a>
-            </button>            
+            </button>             */}
           </div>
         </div>  
       ))}
-
-        
-          
-
-    </div>
+      </div>
+      
   )
 }
